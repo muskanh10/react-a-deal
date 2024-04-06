d0b60e2c0154129cfeb0c91bd009fc8c5fea5c62
muskanh10 <hassanan@usc.edu>
2024-04-05 21:19:36 -0700

    banner

--- src/components/Slider/HeaderSlider.js
+++ src/components/Slider/HeaderSlider.js
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./HeaderSlider.css";
+import { Link } from "react-router-dom";
 
 const HeaderSlider = () => {
   let settings = {
@@ -17,13 +18,43 @@ const HeaderSlider = () => {
     slidesToScroll: 1,
   };
 
+  // return (
+  //   <div className="container-fluid">
+  //     <div className="slider-container">
+  //       <Slider {...settings}>
+  //         {sliderImgs.map((img, index) => (
+  //           <div key={index} className="slider-slide">
+  //             <img src={img} alt={`Slide ${index + 1}`} />
+  //           </div>
+  //         ))}
+  //       </Slider>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="container-fluid">
       <div className="slider-container">
         <Slider {...settings}>
-          {sliderImgs.map((img, index) => (
-            <div key={index} className="slider-slide">
-              <img src={img} alt={`Slide ${index + 1}`} />
+          {sliderImgs.map((slide, slideIndex) => (
+            <div key={slideIndex} className="slider-slide">
+              <div className="image-grid"> {/* Ensure appropriate CSS for grid layout */}
+              <div className="top-row">
+                {slide.images.slice(0,2).map((img, imgIndex) => (
+                
+                  <Link to={`/category/${img.link}`} key={imgIndex} className="m-3 image-link">
+                    <img src={img.src} alt={`Slide ${slideIndex + 1}, Image ${imgIndex + 1}`} />
+                  </Link>
+                ))}
+                </div>
+                <div className="bottom-row">
+                {slide.images.slice(2).map((img, imgIndex) => (
+                
+                <Link to={`/category/${img.link}`} key={imgIndex} className="m-3 image-link">
+                  <img src={img.src} alt={`Slide ${slideIndex + 1}, Image ${imgIndex + 3}`} />
+                </Link>
+              ))}
+                </div>
+              </div>
             </div>
           ))}
         </Slider>
--- src/utils/images.js
+++ src/utils/images.js
@@ -1,7 +1,49 @@
-import slider_img_1 from "../assets/slider_img_1.jpg";
-import slider_img_2 from "../assets/slider_img_2.jpg";
 import shopping_cart from "../assets/shopping_cart.png";
+import laptop from "../assets/laptop.jpg";
+import mens_shirt from "../assets/mens-shirt.jpg";
+import mens_shoes from "../assets/mens-shoes.jpg";
+import mens_watches from "../assets/mens-watches.jpg";
+import smartphones from "../assets/smartphones.jpg";
+import womens_watches from "../assets/womens-watches.jpg";
+import womens_shoes from "../assets/womens-shoes.jpg";
+import womens_bag from "../assets/womens-bag.jpg";
+import womens_dress from "../assets/womens-dress.jpg";
+import womens_jewellery from "../assets/womens-jewellery.jpg";
+import groceries from "../assets/groceries.jpg";
+import sunglasses from "../assets/sunglasses.jpg";
+import perfumes from "../assets/perfumes.jpg";
+import motorcycle from "../assets/motorcycle.jpg"
+import skincare from "../assets/skincare.jpg";
 
  
-const sliderImgs = [slider_img_1, slider_img_2];
+const sliderImgs = [
+    {
+      images: [
+        { src: womens_dress, link: "womens-dresses" },
+        { src: womens_shoes, link: "womens-shoes" },
+        { src: womens_watches, link: "womens-watches" },
+        { src: womens_bag, link: "womens-bags" },
+        { src: womens_jewellery, link: "womens-jewellery" }
+      ],
+    },
+    {
+      images: [
+        { src: mens_shirt, link: "mens-shirts" },
+        { src: mens_shoes, link: "mens-shoes" },
+        { src: mens_watches, link: "mens-watches" },
+        { src: smartphones, link: "smartphones" },
+        { src: laptop, link: "laptops" },
+      ],
+    },
+    {
+        images: [
+          { src: groceries, link: "groceries" },
+          { src: sunglasses, link: "sunglasses" },
+          { src: perfumes, link: "fragnances" },
+          { src: skincare, link: "skincare" },
+          { src: motorcycle, link: "motorcycle" },
+        ],
+      },
+  ];
+  
 export {sliderImgs, shopping_cart};
